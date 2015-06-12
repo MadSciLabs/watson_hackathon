0cb206526be1074ef5d8e9ac727a27ecbd2f43df
Adam Lassy <adam.lassy@havas-79.local>
2015-06-12 09:59:25 -0400

    up

--- watson_server.js
+++ watson_server.js
@@ -1,118 +1,129 @@
 //'use strict';
+var Twitter = require('twitter');
 
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var server;
 
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
 var watson = require('watson-developer-cloud');
 
-var test_text = 'He had numerous movie and TV roles beginning in 1948, including more than 150 on film, often as a villain. In one such role, he appeared as James Bonds nemesis Francisco Scaramanga in 1974s "The Man with the Golden Gun."He had numerous movie and TV roles beginning in 1948, including more than 150 on film, often as a villain. In one such role, he appeared as James Bonds nemesis Francisco Scaramanga in 1974s "The Man with the Golden Gun.He had numerous movie and TV roles beginning in 1948, including more than 150 on film, often as a villain. In one such role, he appeared as James Bonds nemesis Francisco Scaramanga in 1974s "The Man with the Golden Gun.';
+var all_personal_content = "";
+var s_all_tweets = "";
+var s_screen_name = "";
 
-//CREDENTIALS FOR PERSONALITY
-var personality_insights = watson.personality_insights({
-  username: '677aab8a-a949-4784-a47a-702c9c8563bc',
-  password: 'TudtUp7MxQyN',
-  version: 'v2'
+
+//Get Personal Content
+function get_personal_content(_twitter, _content)
+{
+	var client = new Twitter({
+  consumer_key: '25cStyNZOD4ei7j52dQ',
+  consumer_secret: 'Gl9XSYO7MfojGQ9jT10eJsXvlnKkeSMjFut8ttXh1s',
+  access_token_key: '256132595-AhuH9v5fEIe3UHyGfFcSuqoqd14swpuajiSStmL4',
+  access_token_secret: 'jnOSW7VFGgXEN6HNBjUY9vTbgdPY4neas2Qk4zUcZY'
+	});
+
+	var params = {screen_name: _twitter};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+
+  if (!error) {
+
+    //var jsonObj = JSON.parse(tweets);
+
+        for(var myKey in tweets) {
+                s_all_tweets += tweets[myKey]['text'] + "\n";
+                //console.log("key:"+myKey+", value:"+tweets[myKey]['text']);
+        }
+
+        console.log(s_all_tweets);
+
+  	}
+	});
+
+	//Get content from links
+	var arr = _content.split(",");
+	for (var item in arr) {
+ 		console.log(item); 
+	}
+}
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/dashboard.html', function(req,res) {
+
+	res.sendfile("dashboard.html");
+
 });
 
+/****************************
+ ALL TOGETHER 
+***************************/
+app.get('/all', function (req, res) {
 
-//OPTIONS FOR INCOMMING
-server = http.createServer(function(req, res){
-    // your normal server code
-    var path = url.parse(req.url).pathname;
-    switch (path){
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
-            res.end();
-            break;
-        case '/dashboard.html':
-            fs.readFile(__dirname + path, function(err, data){
-                if (err){ 
-                    return send404(res);
-                }
-                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(data, 'utf8');
-                res.end();
-            });
-	    break;
+	//All personal content
+	all_personal_content = get_personal_content(req.param("name"), req.param("samples")); 
+});
 
-	case '/pi':
+/****************************
+  Word Cloud 
+***************************/
+app.get('/wordcloud', function (req, res) {
+
+	//All personal content
+	all_personal_content = get_personal_content(req.param("name"), req.param("samples")); 
+});
+
+/****************************
+  News
+***************************/
+app.get('/news', function (req, res) {
+
+	    console.log(">" + req.param("name") );
+});
+
+/****************************
+  Personality Index
+***************************/
+app.get('/pi', function (req, res) {
+
+	//All personal content
+	all_personal_content = get_personal_content(req.param("name"), req.param("samples")); 
+
+	//CREDENTIALS FOR PERSONALITY
+	var personality_insights = watson.personality_insights({
+  		username: '677aab8a-a949-4784-a47a-702c9c8563bc',
+  		password: 'TudtUp7MxQyN',
+  		version: 'v2'
+	});
+	    console.log(">" + req.param("name") );
+
 	    personality_insights.profile(
-  		{text: test_text},
+  		{text: all_personal_content},
 
   		function (err, response) {
     		if (err) {
       			console.log('error:', err);
-                    	return send404(res);
     		}	
                 
-      		console.log('success : ' + JSON.stringify(response, null, 2));
+      		console.log('success : ');
+      		//console.log('success : ' + JSON.stringify(response, null, 2));
 
 		res.writeHead('Content-Type', 'application/json');
 		res.write(JSON.stringify(response, null, 2));
 		res.end();
-
-		/*
-		res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(response, 'utf8');
-                res.end();
-	        */
-	
-		});
-
-        break;
-        default: send404(res);
-    }
-}),
-
-send404 = function(res){
-    res.writeHead(404);
-    res.write('404');
-    res.end();
-};
-
-server.listen(8080);
-
-/**
-LISTEN FOR SOCKET REUQESTS ... CAN IGNORE
-*/
-
-// use socket.io
-var io = require('socket.io').listen(server);
-
-var _client_light = 0;
-var dirty = true;
-
-//turn off debug
-io.set('log level', 1);
-
-// define interactions with client
-io.sockets.on('connection', function(socket){
-
-    //send data to client
-    setInterval(function(){
-        socket.emit('client_light', {'val': Math.floor((Math.random() * 200) + 1)});
-    }, 100);
-
-    //recieve client data
-    socket.on('rpi_light', function(data){
-
-	dirty = true;
-        //process.stdout.write(data.val + "\n");
-	console.log("in " + data.val);
-        _client_light = data.val; 
-
-    });
-
-    //SEND OUT
-/*
-    setInterval(function(){
-	if (true) { dirty = false;
-        socket.emit('client_light', {'val': _client_light});
-        }
-    }, 10);
-*/
+	});
 
 });
+
+
+app.listen(3000,function () {
+	console.log("started on port 3000");
+});
+
+/*
+LISTEN FOR SOCKET REUQESTS ... CAN IGNORE
+*/
